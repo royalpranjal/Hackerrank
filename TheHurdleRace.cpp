d7803876eea1666d636359e9086f531a4a8b89c8
Pranjal <ug201314013@iitj.ac.in>
2017-06-23 02:04:33 +0530

    The Hurdle Race

---
+++ TheHurdleRace.cpp
@@ -0,0 +1,49 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    int n;
+    int k;
+    cin >> n >> k;
+    vector<int> height(n);
+    for(int height_i = 0; height_i < n; height_i++){
+       cin >> height[height_i];
+    }
+    // your code goes here
+    
+    int ans = 0;
+    
+    for(int i = 0; i < n; i++){
+        if(height[i] > k){
+            ans += (-k + height[i]);
+            k = height[i];
+        }
+    }
+    
+    cout << ans << endl;
+    
+    return 0;
+}
