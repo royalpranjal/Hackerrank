8400995cfd31290134c44ec708c0f4d128604602
Pranjal <ug201314013@iitj.ac.in>
2017-06-24 15:36:26 +0530

    Designer PDF Viewer

---
+++ DesignerPDFViewer.cpp
@@ -0,0 +1,44 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+
+int main(){
+    vector<int> h(26);
+    for(int h_i = 0; h_i < 26; h_i++){
+       cin >> h[h_i];
+    }
+    string word;
+    cin >> word;
+    
+    int height = 0;
+    
+    for(int i = 0; i < word.size(); i++){
+        height = max(height, h[word[i]-'a']);
+    }
+    
+    cout << height*word.size() << endl;
+    
+    return 0;
+}
